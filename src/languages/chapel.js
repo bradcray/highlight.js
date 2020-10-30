aa1c7938d9796dfaf98531a684e26d3cdd19f207
Brad Chamberlain <bradcray@users.noreply.github.com>
2020-10-30 10:39:18 -0700

    Turn off autodetect to pass testing
    ---
    Signed-off-by: Brad Chamberlain <bradcray@users.noreply.github.com>

--- src/languages/chapel.js
+++ src/languages/chapel.js
@@ -9,6 +9,7 @@ export default function(hljs) {
     return {
         name: "Chapel",
         aliases: ['chapel', 'chpl'],
+        disableAutodetect: true,
         keywords: {
             keyword: 'align as atomic begin bool borrowed break by bytes catch class cobegin coforall complex config const continue defer delete dmapped do domain else enum export except extern for forall forwarding if imag import in include index inline inout int iter label lambda let lifetime local locale module new noinit nothing on only otherwise out override owned param private proc prototype public real record reduce ref require return scan select serial shared single sparse string subdomain sync then this throw throws try try! type uint union unmanaged use var void when where while with yield zip',
             built_in: 'here Locales write writef writeln',
